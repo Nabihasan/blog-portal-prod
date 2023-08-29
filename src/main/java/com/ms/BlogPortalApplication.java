a30ef8d26829ea491555d1b4b3f5f76fdd3f4c06
Mohammad Nabi Hasan <nabihasan7976@gmail.com>
2023-08-29 09:04:51 +0530

    commit

--- src/main/java/com/ms/BlogPortalApplication.java
+++ src/main/java/com/ms/BlogPortalApplication.java
@@ -1,7 +1,5 @@
 package com.ms;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,15 +8,11 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 
 public class BlogPortalApplication {
-	private static final Logger logger = LogManager.getLogger(BlogPortalApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogPortalApplication.class, args);
 		
-		logger.info("in main class");
-		logger.info("info logging is printed");
-		logger.debug("logger debud is worked");
-		logger.info("logger warn is worked");
+		
 	}
 	
 	@Bean
